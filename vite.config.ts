cccccb6fdc03bb36168b552460719746508bf0b0
Sh1mpi <meqalion2@gmail.com>
2024-05-19 21:35:22 +0300

    module5

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,49 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-      plugins: [react()],
+      plugins: [
+            react(),
+            VitePWA({
+               registerType: "prompt",
+               includeAssets: ["logo.ico", "apple-touch-icon.png", "masked-icon.svg"],
+               manifest: {
+                  name: "Arsenii",
+                  short_name: "Ars",
+                  description: "Website for client-web-sem4",
+                  theme_color: "#ffffff",
+                  start_url: "/",
+                  icons: [
+                        {
+                              src: 'logo192.png',
+                              sizes: '192x192',
+                              type: 'image/png'
+                        },
+                        {
+                              src: 'logo512.png',
+                              sizes: '512x512',
+                              type: 'image/png'
+                        },
+                        {
+                              src: 'logo512.png',
+                              sizes: '512x512',
+                              type: 'image/png',
+                              purpose: 'any maskable'
+                        }
+                  ],
+               },
+            }),
+      ],
       test: {
             globals: true,
             environment: 'jsdom',
             setupFiles: 'tests/setup.tsx',
             css: true,
       },
+      server: {
+            host: '0.0.0.0',
+            port: 4173
+      }
 });
